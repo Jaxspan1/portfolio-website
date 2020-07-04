b0a9fc0a4ac3063862032aade0bd4c0d709fb544
feyton <tumbafabruce@gmail.com>
2020-07-04 10:01:23 +0200

    Initial commit

---
+++ feyton/settings/local.py
@@ -0,0 +1,10 @@
+from .base import *
+
+SECRET_KEY = config('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
